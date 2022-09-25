0c56292e03a2f6542e6b7275e9c3172b74141ec2
Carlos <cnoram1@my.centennialcollege.ca>
2022-09-25 03:10:04 -0400

    Initial Commit

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -12,8 +12,69 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
     }
 
-
+    @IBAction func BtnClean(_ sender: Any) {
+    }
+    
+    @IBAction func BtnParenthesis(_ sender: Any) {
+    }
+    
+    @IBAction func BtnPercent(_ sender: Any) {
+    }
+    
+    @IBAction func BtnDivide(_ sender: Any) {
+    }
+    
+    @IBAction func BtnSeven(_ sender: Any) {
+    }
+    
+    @IBAction func BtnEight(_ sender: Any) {
+    }
+    
+    @IBAction func BtnNine(_ sender: Any) {
+    }
+    
+    @IBAction func BtnMultiply(_ sender: Any) {
+    }
+    
+    @IBAction func BtnFour(_ sender: Any) {
+    }
+    
+    @IBAction func BtnFive(_ sender: Any) {
+    }
+    
+    @IBAction func BtnSix(_ sender: Any) {
+    }
+    
+    @IBAction func BtnSubtract(_ sender: Any) {
+    }
+    
+    @IBAction func BtnOne(_ sender: Any) {
+    }
+    
+    @IBAction func BtnTwo(_ sender: Any) {
+    }
+    
+    @IBAction func BtnThree(_ sender: Any) {
+    }
+    
+    @IBAction func BtnAdd(_ sender: Any) {
+    }
+    
+    @IBAction func BtnAdd_Subtract(_ sender: Any) {
+    }
+    
+    @IBAction func BtnZero(_ sender: Any) {
+    }
+    
+    @IBAction func BtnDecimal(_ sender: Any) {
+    }
+    
+    @IBAction func BtnEqual(_ sender: Any) {
+    }
+    
+    
 }
 
